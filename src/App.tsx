8cedd732b6e98cf31a085706b98e0fca4bc7131b
Mattias Mucherie <mattias.mattias.m@gmail.com>
2020-12-23 00:00:52 +0100

    --wip-- [skip ci]

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Graticule,
+  Sphere,
+} from 'react-simple-maps'
+import { Geo } from './types/geoTypes'
+
+const geoUrl =
+  'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json'
 
 function App() {
+  const [countryHover, setCountryHover] = useState('')
+  const [countriesBeen, setCountriesBeen] = useState<string[]>([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>You are hovering over {countryHover}</div>
+        <div>You have selected: {countriesBeen.sort()}</div>
+        <ComposableMap data-tip="" projection="geoEqualEarth">
+          <Sphere
+            stroke="#DDD"
+            id="sphere"
+            fill="transparent"
+            strokeWidth={0.5}
+          />
+          <Graticule stroke="#DDD" />
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo: Geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill={countriesBeen.includes(geo.properties.NAME_LONG)?"#ff8552":"#297373"}
+                  onMouseEnter={() => setCountryHover(geo.properties.NAME_LONG)}
+                  onMouseLeave={() => setCountryHover('')}
+                  onMouseDown={() => setCountriesBeen([...countriesBeen, geo.properties.NAME_LONG])}
+                  style={{
+                    default: {
+                      outline: 0
+                    },
+                    hover: {
+                      fill: "#ff8552",
+                      outline: 0
+                    },
+                    pressed: {
+                      fill: "#ff7033",
+                      outline: 0
+                    }
+                  }}
+                />
+              ))
+            }
+          </Geographies>
+        </ComposableMap>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/types/geoTypes.ts
@@ -0,0 +1,21 @@
+export interface Geo {
+  geometry: {}
+  properties: {
+    ABBREV: string
+    CONTINENT: string
+    FORMAL_EN: string
+    GDP_MD_EST: number
+    GDP_YEAR: number
+    ISO_A2: string
+    ISO_A3: string
+    NAME: string
+    NAME_LONG: string
+    POP_EST: number
+    POP_RANK: number
+    POP_YEAR: number
+    REGION_UN: string
+    SUBREGION: string
+  }
+  rsmKey: string
+  svgPath: string
+}
